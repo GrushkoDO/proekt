3ca7973a7ac6dc686fbdcacdd1ce56b3b7b4b7ba
Олег Грушко <grushkoov@yandex.ru>
2022-10-29 09:26:58 +0500

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public delegate void AccountHandler(string message);
+        string fio;
+            int sum;
+            // Создаем переменную делегата
+            AccountHandler taken;
+            public Class1(int sum,string b)
+            {
+                this.sum = sum;
+            fio = b;
+
+            }
+            // Регистрируем делегат
+            public void RegisterHandler(AccountHandler del)
+            {
+                taken = del;
+            }
+            public void Add(int sum) => this.sum += sum;
+            public void Take(int sum)
+            {
+                if (this.sum >= sum)
+                {
+                    this.sum -= sum;
+                    // вызываем делегат, передавая ему сообщение
+                    taken?.Invoke($"Со счета {fio}списано {sum} у.е.");
+                }
+                else
+                {
+                    taken?.Invoke($"Недостаточно средств. Баланс: {this.sum} у.е.");
+                }
+            }
+
+
+        
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1;
+
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            // создаем банковский счет
+            Class1 account = new Class1 (200);
+            Class1 name = new Class1();
+            // Добавляем в делегат ссылку на метод PrintSimpleMessage
+            account.RegisterHandler(PrintSimpleMessage);
+            // Два раза подряд пытаемся снять деньги
+            account.Take(100);
+            account.Take(150);
+
+            void PrintSimpleMessage(string message) => MessageBox.Show(message);
+        }
+    }
+}
